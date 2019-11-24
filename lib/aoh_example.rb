bc71da2c5d3f9a40a9657ab0cb2bafd3eb57b70d
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-11-24 21:46:11 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,19 +7,26 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  # Build an array that contains (or, "nests") the constants into a single
-  # Array. Ruby constants are denoted by ALL_CAPS
+  assembled_aoh = [ { name:  "Don Gately", occupation: "Live-in Staff" }, { name: "Joelle van Dyne", occupation: "Radio Personality"}, {name: "Pat Monteseian", occupation: "Staff"}, { name: "Kate Gampert", occupation: "None"}, { name: "Bruce Green", occupation: "Fan of Mildred"}]
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+DON_G = {name:  "Don Gately", occupation: "Live-in Staff" }
+JOELLE_VD = {name:  "Joelle van Dyne", occupation: "Radio Personality" }
+PAT_M =  {name:  "Pat Monteseian", occupation: "Staff" }
+KATE_G = {name:  "Kate Gompert", occupation: "None" }
+BRUCE_G = {name:  "Bruce Green", occupation: "Fan of Mildred" }
+literal_aoh = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh_lookup = [ { name:  "Don Gately", occupation: "Live-in Staff" }, { name: "Joelle van Dyne", occupation: "Radio Personality"}, {name: "Pat Monteseian", occupation: "Staff"}, { name: "Kate Gampert", occupation: "None"}, { name: "Bruce Green", occupation: "Fan of Mildred"}]
+  aoh_lookup[1][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
-  # Update the AoH data at row and key to have the value of new_value
-  # Return the updated AoH
+  aoh_update = [ { name:  "Don Gately", occupation: "Live-in Staff" }, { name: "Joelle van Dyne", occupation: "Radio Personality"}, {name: "Pat Monteseian", occupation: "Staff"}, { name: "Kate Gampert", occupation: "None"}, { name: "Bruce Green", occupation: "Fan of Mildred"}]
+  aoh_update[0][:occupation] = "Software Engineer"
 end
+aoh_update
