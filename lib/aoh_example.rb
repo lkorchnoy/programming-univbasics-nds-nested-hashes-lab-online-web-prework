5b4dc40f9deb9dd3402fa0fb53a9d62da48f4b0f
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-11-24 21:37:16 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,11 +11,17 @@ def assembled_aoh
 end
 
 def literal_aoh
-  
+DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
+JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" }
+PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
+KATE_G = { name:  "Kate Gompert", occupation: "None" }
+BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+literal_aoh = [ DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
   
 end
 
 def aoh_lookup(aoh, row, key)
+  aoh_lookup = 
 end
 
 def aoh_update(aoh, row, key, new_value)
