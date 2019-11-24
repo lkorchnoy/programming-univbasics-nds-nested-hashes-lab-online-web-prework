e76af7969489e0200a9eb8eb49d41b687bb6821b
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-11-24 21:39:15 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,10 +21,10 @@ literal_aoh = [ DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
 end
 
 def aoh_lookup(aoh, row, key)
-  aoh_lookup = 
+  aoh_lookup = [ { name:  "Don Gately", occupation: "Live-in Staff" }, { name: "Joelle van Dyne", occupation: "Radio Personality"}, {name: "Pat Monteseian", occupation: "Staff"}, { name: "Kate Gampert", occupation: "None"}, { name: "Bruce Green", occupation: "Fan of Mildred"}]
+  aoh_lookup[1][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
-  # Update the AoH data at row and key to have the value of new_value
-  # Return the updated AoH
+  aoh_update = 
 end
